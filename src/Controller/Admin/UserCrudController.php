368375b3f32f8a1611d77e6c2e88f8ad67690017
MWJulien <j.fournier@mentalworks.fr>
2024-04-28 20:02:37 +0200

    fix crud for us

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -4,9 +4,12 @@ namespace App\Controller\Admin;
 
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 
 class UserCrudController extends AbstractCrudController
 {
@@ -15,14 +18,25 @@ class UserCrudController extends AbstractCrudController
         return User::class;
     }
 
-
     public function configureFields(string $pageName): iterable
     {
+        $passwordField = TextField::new('password', 'Password')
+            ->hideOnIndex();
+
+        if ($pageName === \EasyCorp\Bundle\EasyAdminBundle\Config\Crud::PAGE_EDIT || $pageName === \EasyCorp\Bundle\EasyAdminBundle\Config\Crud::PAGE_NEW) {
+            $passwordField->setFormType(PasswordType::class);
+        }
+
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(), // Hide ID in form as it's not typically editable
+            TextField::new('firstname', 'First Name'),
+            TextField::new('name', 'Last Name'),
+            EmailField::new('email', 'Email'),
+            ArrayField::new('roles', 'Roles')
+                ->setHelp('Roles assigned to the user, e.g., ROLE_USER, ROLE_ADMIN'),
+            $passwordField,
+            BooleanField::new('enabled', 'Enabled')
         ];
     }
-
 }
+
---
+++ src/Entity/CompanyResponsible.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Entity;
+
+use App\Entity\Traits\EnabledTrait;
+use App\Entity\Traits\SoftDeleteTrait;
+use App\Entity\Traits\TimestampableTrait;
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+#[ORM\Entity(repositoryClass: CompanyResponsibleRepository::class)]
+#[ORM\Table(name: '`Company_responsible`')]
+class CompanyResponsible
+{
+    use TimestampableTrait;
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    private ?string $position = null;
+
+    private ?string $phone = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPosition(): ?string
+    {
+        return $this->position;
+    }
+
+    public function setPosition(string $position): static
+    {
+        $this->position = $position;
+
+        return $this;
+    }
+
+    public function getPhone(): ?string
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(string $phone): static
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+}
