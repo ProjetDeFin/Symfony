b8638de2a70b929bb4ddea87562fdeb311b5b0f4
Tassadar921 <paul.lecuisinier@gmail.com>
2024-06-02 17:30:09 +0200

    patch

--- src/Service/ApiResponseService.php
+++ src/Service/ApiResponseService.php
@@ -10,11 +10,12 @@ final readonly class ApiResponseService
         private string $frontUrl,
     ) {
     }
+
     public function getResponse(): JsonResponse
     {
         $response = new JsonResponse();
         $response->headers->set('Access-Control-Allow-Origin', $this->frontUrl);
-        $response->headers->set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, DELETE');
+        $response->headers->set('Access-Control-Allow-Methods', 'POST, GET');
         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept');
         return $response;
     }
