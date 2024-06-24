dc5478198246142950e0749fceee393e0543dbc2
Tassadar921 <paul.lecuisinier@gmail.com>
2024-06-24 14:20:11 +0200

    test

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
@@ -17,6 +17,7 @@ class SecurityController extends AbstractController
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
+        // Check if the user is already logged in
         if ($this->security->getUser()) {
             return $this->redirectToRoute('admin');
         }
