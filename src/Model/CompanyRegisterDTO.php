c8310c0566a6d106562a40e2d83367b02b3b0dee
Tassadar921 <paul.lecuisinier@gmail.com>
2024-06-30 15:41:48 +0200

    new entities

---
+++ src/Model/CompanyRegisterDTO.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Model;
+
+use App\Enum\UserGenderEnum;
+
+class CompanyRegisterDTO {
+    private string $name;
+    private string $siret;
+    private string $activity;
+
+}
